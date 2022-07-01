32ed0f3af874894a81f5417c95a95f9b4acde9c1
Alvin Sanchez <alvin.sanchez27@gmail.com>
2022-07-01 01:47:31 -0400

    Lab Complete

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
-// Write your solution in this file!
+const employee = {
+    name: "Alvin",
+    streetAddress: "123 lone ln"
+    }
+
+
+function updateEmployeeWithKeyAndValue(employee, key, value){
+    return {
+        ...employee,
+        [key]: value,
+    };
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value){
+    employee[key] = value
+    return employee;
+}
+
+function deleteFromEmployeeByKey(employee, key){
+    const newEmployee = {...employee};
+     
+    delete employee.key;
+
+    newEmployee[key] = employee.key;
+
+    return newEmployee;
+}
+function destructivelyDeleteFromEmployeeByKey(employee, key){
+   delete employee.key
+   employee[key] = employee.kay;
+   return employee;
+}
